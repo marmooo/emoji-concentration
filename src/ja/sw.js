8b4f801990b16bc1c591a419ed76ad220137efcf
marmooo <marmooo@users.noreply.github.com>
2022-11-26 13:00:58 +0900

    bump bootstrap from 5.2.2 to 5.2.3

--- src/ja/sw.js
+++ src/ja/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-10-24 00:35";
+var CACHE_NAME = "2022-11-26 12:58";
 var urlsToCache = [
   "/emoji-concentration/",
   "/emoji-concentration/index.js",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/emoji-concentration/mp3/correct3.mp3",
   "/emoji-concentration/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
