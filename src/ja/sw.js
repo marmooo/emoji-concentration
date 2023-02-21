857fdfe4a85b973c5880eaea61e5681b2773e9c0
marmooo <marmooo@users.noreply.github.com>
2023-02-21 09:42:20 +0900

    remove iOS joke voices

--- src/ja/sw.js
+++ src/ja/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-11-26 12:58";
+var CACHE_NAME = "2023-02-21 09:40";
 var urlsToCache = [
   "/emoji-concentration/",
   "/emoji-concentration/index.js",
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-12-31 00:50";
+var CACHE_NAME = "2023-02-21 09:40";
 var urlsToCache = [
   "/emoji-concentration/",
   "/emoji-concentration/index.js",
