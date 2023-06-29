e4a274e77b4d10b499f20aa4483ac3b8c28cb9d8
marmooo <marmooo@users.noreply.github.com>
2023-06-30 00:02:29 +0900

    improve dark-mode

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ loadConfig();
 
 function loadConfig() {
   if (localStorage.getItem("darkMode") == 1) {
-    document.documentElement.dataset.theme = "dark";
+    document.documentElement.setAttribute("data-bs-theme", "dark");
   }
   if (originalLang == "ja") {
     if (localStorage.getItem("furigana") == 1) {
@@ -28,10 +28,10 @@ function loadConfig() {
 function toggleDarkMode() {
   if (localStorage.getItem("darkMode") == 1) {
     localStorage.setItem("darkMode", 0);
-    delete document.documentElement.dataset.theme;
+    document.documentElement.setAttribute("data-bs-theme", "light");
   } else {
     localStorage.setItem("darkMode", 1);
-    document.documentElement.dataset.theme = "dark";
+    document.documentElement.setAttribute("data-bs-theme", "dark");
   }
 }
 
