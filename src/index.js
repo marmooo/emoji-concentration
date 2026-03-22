7aed48d24e3b2b76d260ffd9fa4190929cdcc3dc
marmooo <marmooo@users.noreply.github.com>
2026-03-22 16:03:46 +0900

    improve dark-mode

--- src/index.js
+++ src/index.js
@@ -7,22 +7,14 @@ let selected = document.getElementById("choices").children[2];
 let englishVoices = [];
 let audioContext;
 const audioBufferCache = {};
-loadConfig();
-
-function loadConfig() {
-  if (localStorage.getItem("darkMode") == 1) {
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
-}
 
 function toggleDarkMode() {
-  if (localStorage.getItem("darkMode") == 1) {
-    localStorage.setItem("darkMode", 0);
-    document.documentElement.setAttribute("data-bs-theme", "light");
-  } else {
-    localStorage.setItem("darkMode", 1);
-    document.documentElement.setAttribute("data-bs-theme", "dark");
-  }
+  const html = document.documentElement;
+  const newTheme = html.getAttribute("data-bs-theme") === "dark"
+    ? "light"
+    : "dark";
+  html.setAttribute("data-bs-theme", newTheme);
+  localStorage.setItem("darkMode", newTheme);
 }
 
 function changeLang() {
